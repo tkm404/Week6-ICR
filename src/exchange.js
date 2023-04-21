b12c332a71ad90f147157731111749e4903263b3
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-21 08:34:42 -0700

    set up API call business logic.

---
+++ src/exchange.js
@@ -0,0 +1,16 @@
+export default class ExchangeService {
+static getExchange(usd) {
+  return fetch()
+  .then(function(response) {
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`;
+        throw new Error(errorMessage);
+      } else {
+        return response.json();
+      }
+    })
+    .catch(function(error) {
+      return error;
+    });
+  }
+}
