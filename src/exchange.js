1912b306824d5c029274240f022fe1f5eb4db3bc
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-21 09:01:00 -0700

    stage GET request via fetch method.

--- src/exchange.js
+++ src/exchange.js
@@ -1,6 +1,6 @@
 export default class ExchangeService {
 static getExchange(usd) {
-  return fetch(``)
+  return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
   .then(function(response) {
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
