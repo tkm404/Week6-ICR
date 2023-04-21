053279d261d8b9bf7504b6e16f6888609eea9f33
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-21 11:13:57 -0700

    add method for calculating result in Calculator logic.

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,10 @@
 export default class Calculator {
-  
+  constructor(ammount, selection) {
+    this.ammount = ammount
+    this.selection = selection
+  }
+ static exchangeResult(ammount, selection) {
+ const result = ammount*selection
+ return result
+ }
 }
